2f1bfd71ff5f9d04b91979a847a38fb69ab812c6
Morgan Houppin <mhouppin@z2r6p4.le-101.fr>
2019-05-23 17:06:57 +0200

    Started instruction execution

---
+++ project/bit_mask.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   bit_mask.h                                       .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mhouppin <mhouppin@le-101.fr>              +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/05/23 13:33:36 by mhouppin     #+#   ##    ##    #+#       */
+/*   Updated: 2019/05/23 16:08:20 by mhouppin    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef BIT_MASK_H
+# define BIT_MASK_H
+
+#define BIT_15		0x8000u
+#define BIT_11		0x800u
+#define BIT_7		0x80u
+#define BIT_6		0x40u
+#define BIT_5		0x20u
+#define BIT_4		0x10u
+#define BIT_3		0x8u
+#define BIT_2		0x4u
+#define BIT_1		0x2u
+#define BIT_0		0x1u
+
+#define BITS_0123	0x0Fu
+
+#endif
