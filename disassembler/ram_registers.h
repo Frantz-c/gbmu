47e147d2c57a82d4c5256285214a84a9ac7829f1
Frantz Cordon <fcordon@z2r5p3.le-101.fr>
2019-05-24 19:35:47 +0200

    petite avancee... rfile (hexdump perso)

--- disassembler/ram_registers.h
+++ disassembler/ram_registers.h
@@ -1,3 +1,16 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ram_registers.h                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: fcordon <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/05/24 17:07:32 by fcordon      #+#   ##    ##    #+#       */
+/*   Updated: 2019/05/24 19:17:32 by fcordon     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
 #ifndef RAM_REGISTERS_H
 # define RAM_REGISTERS_H
 
@@ -40,19 +53,78 @@
 # define SCX	0xff43U //scroll x
 # define LY		0xff44U //lcdc y coordinate
 # define LYC	0xff45U //LY compare
-# define BCPS	0xff68U //specifies a BG write
-# define BCPD	0xff69U //specifies the BG write data
-# define OCPS	0xff6AU //specifies the OBJ write data
-# define OCPD	0xff6bU //OBJ write data
 # define WY		0xff4aU //window y coordinate
 # define WX		0xff4bU //window x coordinate
 
 // OAM REGISTERS
+# define OBJ_Y(n)		0xfe00U + ((unsigned int)n << 2) //Y coordinate
+# define OBJ_X(n)		0xfe01U + ((unsigned int)n << 2) //X coordinate
+# define OBJ_CHR(n)		0xfe02U + ((unsigned int)n << 2) //CHR code
+# define OBJ_ATTR(n)	0xfe03U + ((unsigned int)n << 2) //Attributes flags
 
-/**** AREA ****/
+// DMA REGISTERS
+# define DMA	0xff46U //DMA transfer register 
+	// (value = starting addr : (uint8_t)(uint16_t >> 8))    for 0xc0000, DMA = 0xc0
+	// [ RAM area (0x8000-0xdfff) to OAM (0xfe00-0xfe9f) ]
+# define HDMA5	0xff55U //new DMA transfer register (CGB)
+	// transfer start + number of bytes to transfer
+
+// LCD COLOR DISPLAY
+# define BCPS	0xff68U //specifies a BG write			(CGB)
+# define BCPD	0xff69U //specifies the BG write data	(CGB)
+# define OCPS	0xff6AU //specifies the OBJ write data	(CGB)
+# define OCPD	0xff6bU //OBJ write data				(CGB)
+
+// SOUND 1 REGISTER
+# define NR10	0xff10U //frequency + sound color?
+# define NR11	0xff11U //sound length?
+# define NR12	0xff12U //cresc. decresc. effects? + volume?
+# define NR13	0xff13U //???
+# define NR14	0xff14U //???
+
+// SOUND 2 REGISTER
+# define NR21	0xff16U //frequency + sound color?
+# define NR22	0xff17U //Envelope
+# define NR23	0xff18U //??
+# define NR24	0xff19U //??
+
+// SOUND 3 REGISTER
+# define NR30	0xff1aU // stop/enable sound 3
+# define NR31	0xff1bU // sound length
+# define NR32	0xff1cU // output level
+# define NR33	0xff1dU // low-order frequency data
+# define NR34	0xff1eU // ???
+
+// SOUND 4 REGISTER
+# define NR41	0xff20U // sound length
+# define NR42	0xff21U // envelope
+# define NR43	0xff22U // ???
+# define NR44	0xff23U // ???
+
+// SOUND CONTROL REGISTERS
+# define NR50	0xff24U // SO1 output level [3 bits], Vin -> SO1 on/off [1 bit]
+						// SO2 output level [3 bits], Vin -> SO2 on/off [1 bit]
+# define NR51	0xff25U // Select sound output terminal:
+						// sound 4 to SO2 [1 bit], sound 3 to SO2 [1 bit], sound 2 to SO2 [1 bit], sound 1 to SO2 [1 bit], 
+						// sound 4 to SO1 [1 bit], sound 3 to SO1 [1 bit], sound 2 to SO1 [1 bit], sound 1 to SO1 [1 bit], 
+# define NR52	0xff26U // Disable/enable All sound circuits [1 bit], NULL [3 bits]
+						// sound 4 On flag [1 bit], sound 3 On flag [1 bit], sound 2 On flag [1 bit], sound 1 On flag [1 bit], 
+
+/**** BG & OBJ AREA ****/
 
 // BACKGROUND DISPLAY
-# define BG_DISPLAY_START	0x9800U // or 0x9c00U
-# define BG_DISPLAY_END		0x9bffU // or 0x9fffU
+# define BG_DISPLAY_DATA_1_START	0x9800U
+# define BG_DISPLAY_DATA_1_END		0x9bffU
+# define BG_DISPLAY_DATA_2_START	0x9c00U
+# define BG_DISPLAY_DATA_2_END		0x9fffU
+	//(use of data 1 or 2 is determined by bit 3 of the LCDC register)
+
+// OBJECTS DISPLAY (max 40 OBJ : max 10 OBJ per line)
+# define OAM_DISPLAY_START			0xfe00U
+# define OAM_DISPLAY_END			0xfe9fU
+	//OBJ	 =	y [8 bits], x [8 bits], CHR code [8 bits], display priority [1 bit], 
+	//			vertical flip [1 bit], horizontal flip [1 bit], DMG-mode palette [1 bit],
+	//			character bank [1 bit] (CGB), color palette [3 bits] (CGB).
+	//	total = 32 bits
 
 #endif
---
+++ disassembler/read_file.c
@@ -0,0 +1,288 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   read_file.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: fcordon <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/05/24 14:40:04 by fcordon      #+#   ##    ##    #+#       */
+/*   Updated: 2019/05/24 19:34:36 by fcordon     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <ctype.h>
+
+static inline const char		*left_trim(const char *s)
+{
+	while (*s == ' ' || *s == '\t')
+		s++;
+	if (*s == '0' && s[1] == 'x')
+	{
+		s += 2;
+		while (*s == '0')
+			s++;
+		return (s);
+	}
+	return (NULL);
+}
+
+static unsigned int				get_base_value(char c)
+{
+	if (c >= 'a' && c <= 'f')
+		return (c - ('a' - 10));
+	if (c >= 'A' && c <= 'F')
+		return (c - ('a' - 10));
+	return (c - '0');
+}
+
+static inline unsigned int		ft_strtoi(const char *s)
+{
+	unsigned int	n;
+
+	n = 0;
+	while (1)
+	{
+		if (*s > 'f' || (*s > 'F' && *s < 'a')
+				|| (*s > '9' && *s < 'A') || *s < '0')
+			break ;
+		n *= 16;
+		n += get_base_value(*(s++));
+	}
+	return (n);
+}
+
+extern inline unsigned int		atoi_hexa(const char *s)
+{
+	if ((s = left_trim(s)) == NULL)
+		return (0);
+	return (ft_strtoi(s));
+}
+
+unsigned char	*get_file_contents(const char *file, unsigned int *length)
+{
+	unsigned char		*content;
+	const int	fd = open(file, O_RDONLY);
+	struct stat	filestat;
+
+	if (fd == -1)
+	{
+		fprintf(stderr, "can't open file %s\n", file);
+		return (NULL);
+	}
+	lstat(file, &filestat);
+	if (filestat.st_size == 0)
+	{
+		fprintf(stderr, "empty file\n");
+		return (NULL);
+	}
+	if ((content = malloc(filestat.st_size + 1)) != NULL)
+	{
+		*length = read(fd, content, filestat.st_size);
+		close(fd);
+	}
+	return (content);
+}
+
+void	print_help(const char *prog)
+{
+		fprintf(stderr, "%s \"file\" [option]? [start offset] [end offset]\n\n"
+						"options:\n"
+						"    --bin: binary\n"
+						"    --hex: hexa\n"
+						"    --dec: decimal\n"
+						"    --chr: ascii\n"
+						"    --mixed: hexa + ascii\n\n", prog);
+}
+
+char	*get_bin(unsigned char n)
+{
+	static char					buf[128] = {0};
+	unsigned char				curs;
+	unsigned int				i = 0;
+
+	curs = 1UL << 7;
+	while (curs)
+	{
+		buf[i++] = (curs & n) ? '1' : '0';
+		curs >>= 1;
+	}
+	buf[i] = 0;
+	return (buf);
+}
+
+char	*get_hex(unsigned char n)
+{
+	static char	buf[3];
+
+	buf[2] = 0;
+	buf[1] = n % 16 + '0';
+	if (buf[1] > '9')
+		buf[1] += 39;
+	n /= 16;
+	buf[0] = n + '0';
+	if (buf[0] > '9')
+		buf[0] += 39;
+
+	return (buf);
+}
+
+void	print_addr(unsigned int addr, int base)
+{
+	static char	*fmt = NULL;
+
+	if (!fmt)
+	{
+		switch (base)
+		{
+			case 2:
+			case 'h':
+			case 16: fmt = "\e[0;33m0x%x:\e[0m    "; break;
+			case 'c':
+			case 10: fmt = "\e[0;33m%u:  \e[0m    "; break;
+		}
+	}
+	printf(fmt, addr);
+}
+
+void	print_line(char *buf, int base, unsigned int addr)
+{
+	if (*buf)
+		puts(buf);
+	print_addr(addr, base);
+}
+
+unsigned int	add_octet(char *buf, unsigned int i, const unsigned char *byte, int base)
+{
+	static char	*fmt = NULL;
+
+	if (!fmt)
+	{
+		switch (base)
+		{
+			case 2: fmt = (void*)1; break;
+			case 10: fmt = "%u "; break;
+			case 16: fmt = "%x "; break;
+			case 'c': fmt = "%c"; break;
+		}
+	}
+	if (base == 2)
+		return (i + sprintf(buf + i, "%s ", get_bin(*byte)));
+	else if (base == 'c')
+	{
+	   	if (!isprint(*byte))
+			return (i + sprintf(buf + i, " \e[1;35m%u\e[0m ", *byte));
+		return (i + sprintf(buf + i, "%c", *byte));
+	}
+	else if (base == 16)
+		return(i + sprintf(buf + i, "%s ", get_hex(*byte))); 
+	else if (base == 'h')
+	{
+	   	if (!isprint(*byte))
+			return (i + sprintf(buf + i, "%s ", get_hex(*byte)));
+		return (i + sprintf(buf + i, "\e[0;33m%c\e[0m  ", *byte));
+	}
+	return (i + sprintf(buf + i, "%u ", *byte));
+}
+
+void	print_file(unsigned char *file, unsigned int start, unsigned int end, int base)
+{
+	char			buf[512] = {0};
+	unsigned int	i;
+
+	i = 0;
+	if (start & 0xf)
+		print_addr(start, base);
+	while (start < end)
+	{
+		if ((start & 0xf) == 0)
+		{
+			print_line(buf, base, start);
+			i = 0;
+		}
+		i = add_octet(buf, i, file + start, base);
+		start++;
+	}
+	if (i)
+		print_line(buf, base, start);
+}
+
+int		main(int ac, char *av[])
+{
+	unsigned char	*file;
+	unsigned int	len;
+	unsigned int	start, end;
+	int				base;
+	unsigned int	i = 1;
+
+	if (ac < 2 || ac > 5)
+	{
+		print_help(av[0]);
+		return (1);
+	}
+	base = 0;
+	if (strcmp(av[1], "--bin") == 0)
+		base = 2;
+	else if (strcmp(av[1], "--hex") == 0)
+		base = 16;
+	else if (strcmp(av[1], "--dec") == 0)
+		base = 10;
+	else if (strcmp(av[1], "--chr") == 0)
+		base = 'c';
+	else if (strcmp(av[1], "--mixed") == 0)
+		base = 'h';
+	if (base)
+		i++;
+	else
+	{
+		if (ac == 5)
+		{
+			print_help(av[0]);
+			return (1);
+		}
+	}
+
+	if ((file = get_file_contents(av[i], &len)) == NULL)
+		return (1);
+	start = 0U;
+	end = len;
+	if (ac > i + 1)
+	{
+		if (av[i + 1][0] == '0' && av[i + 1][1] == 'x')
+			start = (unsigned int)atoi_hexa(av[i + 1]);
+		else
+			start = (unsigned int)atoi(av[i + 1]);
+	}
+	if (ac > i + 2)
+	{
+		if (av[i + 2][0] == '0' && av[i + 2][1] == 'x')
+			end = (unsigned int)atoi_hexa(av[i + 2]);
+		else
+			end = (unsigned int)atoi(av[i + 2]);
+	}
+	if (len < end)
+	{
+		fprintf(stderr, "end offset (%d) > file end (%d)\n", end, len);
+		return (1);
+	}
+	if (start > end)
+	{
+		fprintf(stderr, "start offset (%d) > end offset (%d)\n", start, end);
+		return (1);
+	}
+	if (!base)
+		base = 16;
+
+	print_file(file, start, end, base);
+	puts("");
+
+	return (0);
+}
