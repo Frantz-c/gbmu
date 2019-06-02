ed78bd8270ee019a1af82e36f354b6b0b2aac25e
frantz-c <frantz-c@live.fr>
2019-06-02 15:07:58 +0200

    ram_registers: 色んな説明

--- tests/ram_registers.h
+++ tests/ram_registers.h
@@ -101,6 +101,7 @@ exemple:
 # define DIV	0xff04U //????
 /*
 	ecrire dans ce registre le remet a zero
+	il est automatiquement incremente toutes les ?µs
 */
 # define TIMA	0xff05U // TIMER [VVVVVVVV]
 /*
@@ -185,6 +186,7 @@ exemple:
 ************************/
 # define SB		0xff01U
 # define SC		0xff02U
+// On s'en balance !
 
 
 
@@ -271,9 +273,7 @@ _NO_SWITCH:
 **** INFRARED PORT REGISTER (CGB) ****
 *************************************/
 # define RP		0xff56U
-/*
-	On s'en fout !!!!!!!
-*/
+// On s'en fout !!!!!!!
 
 
 
@@ -302,34 +302,238 @@ _NO_SWITCH:
 
 
 
-// LCD DISPLAY REGISTERS
-# define LCDC	0xff40U
-# define STAT	0xff41U
-# define SCY	0xff42U //scroll y
-# define SCX	0xff43U //scroll x
-# define LY		0xff44U //lcdc y coordinate
-# define LYC	0xff45U //LY compare
-# define WY		0xff4aU //window y coordinate
-# define WX		0xff4bU //window x coordinate
+/******************************
+**** LCD DISPLAY REGISTERS ****
+******************************/
+# define LCDC	0xff40U // [76543210]
+/*
+	0 = BG display.
+		1 = on
+		0 = off (if CGB it's always on)
+	1 = OBJ.
+		1 = on
+		0 = off
+	2 = OBJ block size.
+		1 = 8*16
+		0 = 8*8
+	3 = BG code area.
+		1 = 0x9c00 - 0x9fff,
+		0 = 0x9800 - 0x9bff
+	4 = BG chr data area
+		1 = 0x8000 - 0x8fff
+		0 = 0x8800 - 0x97ff
+	5 = Windowing enable flag
+		1 = on
+		0 = off
+	6 = Window code area
+		1 = 0x9c00 - 0x9fff
+		0 = 0x9800 - 0x9bff
+	7 = LCD enable flag
+		1 = on
+		0 = off
 
-// OAM REGISTERS
+
+	details BG, window, obj, ...
+
+
+*/
+
+# define STAT	0xff41U // [.LOVHFMM]
+/*
+	MM = Mode flag (read-only)
+		00 = in horizontal blanking	(cpu can access to VRAM + OAM)
+		01 = in vertical blanking	(cpu can access to VRAM + OAM)
+		10 = reading OAM			(cpu cannot access to OAM)
+		11 = reading both OAM VRAM	(cpu cannot acces to OAM + VRAM)
+	
+	F  = Coincidence flag (read-only)
+		1 = LYC == LY
+		0 = LYC != LY
+	
+	H = H-blank interrupt enable flag
+		1 = on
+		0 = off
+	V = V-blank interrupt flag
+		1 = on
+		0 = off
+	O = OAM interrupt flag
+		1 = on
+		0 = off
+	L = LYC=LY coincidence interrupt
+		1 = on
+		0 = off
+
+	detail routine affichage LCD :
+````````````````````````````````
+while (1)
+{
+	times(144)
+	{
+		10: lecture de l'OAM				(19µs)
+		11: lecture de l'OAM + lecture VRAM	(40-60µs)
+		00: horizontal blank: fin de ligne	(20-49µs)
+	}
+	01: vertical blank						(1087µs)
+}
+*/
+
+# define SCY	0xff42U // [vvvvvvvv] scroll y (R/W)
+/*
+	position y dans le background (32*32 tiles)
+	le background est circulaire.
+*/
+# define SCX	0xff43U // [vvvvvvvv] scroll x (R/W)
+/*
+	position x dans le background (32*32 tiles)
+	le background est circulaire.
+*/
+# define LY		0xff44U // [vvvvvvvv] lcdc y coordinate (read-only)
+/*
+	0 - 153 (144-153 = v-blank)
+*/
+# define LYC	0xff45U // [vvvvvvvv] LY compare (R/W)
+/*
+	LY to compare register (LYC == LY interrupt, ...)
+*/
+# define BGP	0xff47U // [33221100] BG palette (R/W), non CGB-only
+/*
+	00 = shade for color number 0
+	11 = shade for color number 1
+	22 = shade for color number 2
+	33 = shade for color number 3
+
+	00 -> white
+	01 -> light gray
+	10 -> dark gray
+	11 -> black
+*/
+# define OBP0	0xff48U // [332211..] OBJ palette 0 (R/W), non CGB-only
+/*
+	same as BGP (but color number 0 is transparent)
+*/
+# define OBP1	0xff49U // [332211..] OBJ palette 1 (R/W), non CGB-only
+/*
+	same as BGP (but color number 0 is transparent)
+*/
+
+# define WY		0xff4aU // [vvvvvvvv] window y coordinate (R/W)
+/*
+	0 - 143 = visible
+	(modulo de 8 applique ?)
+*/
+# define WX		0xff4bU // [vvvvvvvv] window x coordinate (R/W)
+/*
+	0 - 166 = visible (bug hardware, real value = WX - 7)
+	(modulo de 8 applique ?)
+*/
+
+/*********************************
+**** CGB ONLY COLOR REGISTERS ****
+*********************************/
+#define BGPI	0xff68 // same as BCPS
+#define BCPS	0xff68 // [w.xxxyyz]
+/*
+	
+*/
+
+
+
+
+
+
+
+/*******************
+***** OAM AREA *****
+*******************/
 # define OBJ_Y(n)		0xfe00U + ((unsigned int)n << 2) //Y coordinate
 # define OBJ_X(n)		0xfe01U + ((unsigned int)n << 2) //X coordinate
 # define OBJ_CHR(n)		0xfe02U + ((unsigned int)n << 2) //CHR code
 # define OBJ_ATTR(n)	0xfe03U + ((unsigned int)n << 2) //Attributes flags
+// n = 0-40
 
-// DMA REGISTERS
-# define DMA	0xff46U //DMA transfer register 
-	// (value = starting addr : (uint8_t)(uint16_t >> 8))    for 0xc0000, DMA = 0xc0
-	// [ RAM area (0x8000-0xdfff) to OAM (0xfe00-0xfe9f) ]
+
+
+
+
+
+
+/********************
+*** DMA REGISTERS ***
+********************/
+# define DMA	0xff46U // [ssssssss] DMA transfer register 
+/*
+	ssssssss = source (0xss00 - 0xss9f)
+				min = 0x80 (CGB = 0x00)
+				max = 0xf1
+	
+	destination = 0xfe00 - 0xfe9f (OAM)
+	
+	transfer time = 160µs (60µs in double speed)
+
+	write to this register starts the transfer
+
+	exemple:
+````````````````````````````````````````````````````
+%define DMA		0xff46
+
+3e c0			ld		A, 0xc0
+e0 46			ld		(DMA), A	; start address to DMA
+3e 28			ld		A, 40		; wait 4 x 40 cycles (~160µs)
+3d		wait:	dec		A
+20 fd			jrnz	wait
+c9				ret
+Example of program that writes the above instructions to
+internal RAM starting from 0xff80:
+
+		ld		C, 0x80
+		ld		B, 10
+		ld		HL, DMA_DATA	; code address
+copy:	ld		A, (HL++)
+		ld		(ff00+C), A		; copy to HRAM
+		inc		C
+		dec		B
+		jrnz	copy
+		call	0xff80			; ??????? maybe
+
+.DMA_DATA	db 0x3e, 0xc0, 0xe0, 0x46, 0x3e
+			db 0x28, 0x3d, 0x20, 0xfd, 0xc9
+
+*/
+
+# define HDMA1	0xff51U // [hhhhhhhh] source high
+# define HDMA2	0xff52U // [llllllll] source low
+/*
+	source = 0xhhll & 0xfff0 (last four bits ignored)
+			source = 0x0000-0x7ff0, 0xa000-0xdff0
+	
+	source == VRAM cause garbage to be copied
+*/
+
+# define HDMA3	0xff53U // [hhhhhhhh] destination high
+# define HDMA4	0xff54U // [llllllll] destination low
+/*
+	destination = 0xhhll & 0xfff0 (last four bits ignored)
+			destination = 0x8000-0x9ff0
+*/
+
 # define HDMA5	0xff55U //new DMA transfer register (CGB)
-	// transfer start + number of bytes to transfer
+/*
+	cpu is halted during new DMA transfer.
+	writing to this register starts the transfer.
+	16 bytes transfer takes 8µs
+*/
 
-// LCD COLOR DISPLAY
-# define BCPS	0xff68U //specifies a BG write			(CGB)
-# define BCPD	0xff69U //specifies the BG write data	(CGB)
-# define OCPS	0xff6AU //specifies the OBJ write data	(CGB)
-# define OCPD	0xff6bU //OBJ write data				(CGB)
+
+
+
+
+
+
+
+
+
+
+
 
 // SOUND 1 REGISTER
 # define NR10	0xff10U //frequency + sound color?
