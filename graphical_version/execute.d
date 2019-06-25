6ca8e3f310b94aecc0d70eb8f975b4b07a418a59
Morgan Houppin <mhouppin@z2r6p4.le-101.fr>
2019-06-25 15:29:16 +0200

    Dont push binaries or dependencies...

--- graphical_version/execute.d
+++
@@ -1,2 +0,0 @@
-execute.o: execute.c memory_map.h ram_registers.h debug_tools.h execute.h \
-  registers.h ../tests/memory_map.h ../disassembler/disassemble_table.c
--- graphical_version/load_cartridge.d
+++
@@ -1,2 +0,0 @@
-load_cartridge.o: load_cartridge.c memory_map.h ram_registers.h \
-  debug_tools.h
