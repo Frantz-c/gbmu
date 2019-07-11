6d919f9342e9fd70ca75662618f8fa687fa6fe39
frantz-c <frantz-c@live.fr>
2019-07-11 09:13:36 +0200

    header .go

---
+++ assembler/fichier_objet.c
@@ -0,0 +1,12 @@
+// HEADER
+
+uint32_t	header_length;
+uint32_t	code_length;
+
+// labels extern
+char		*label;		// nom du label
+uint32_t	pos;		// position relative au debut du code (apres le header)
+...
+
+// code
+...
